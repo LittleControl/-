4efa4a88d04c865420ecd4798c6a1ce07f636ee1
LittleControl <littlecontrol@qq.com>
2019-06-10 22:06:37 +0800

    主页以及登录页的框架

--- Main/files/router.js
+++ Main/files/router.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router();
 router.get('/',function(req,res){
-    res.send('Hello,Pop Music!')
+    res.render('index.html')
 })
 module.exports = router
