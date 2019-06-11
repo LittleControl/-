91f661284bf22583d88e55bb1ac6fafb7951d6c7
LittleControl <littlecontrol@qq.com>
2019-06-11 08:20:01 +0800

    数据库的连接

--- Main/files/router.js
+++ Main/files/router.js
@@ -1,6 +1,58 @@
 var express = require('express')
-var router = express.Router();
-router.get('/',function(req,res){
-    res.render('index.html')
+var fs = require('fs')
+var path = require('path')
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true }, function (err) {
+    if (err) {
+        console.log('MongoDB Connected Failed !')
+    } else {
+        console.log('MongoDB Connetced Successfully !')
+    }
 })
+
+var account = mongoose.Schema({
+    email: {
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    }
+})
+var Account = mongoose.model('Account', account)
+var newUser = new Account({
+    email: 'littlecontrol@qq.com',
+    password: 'Nothing'
+})
+newUser.save(function (err, data) {
+    if (err) {
+        console.log('Saved Failed,Something is wrong !')
+    } else {
+        console.log('Save Successfully !')
+        // console.log(data)
+    }
+})
+
+
+var router = express.Router()
+router.get('/', function (req, res) {
+    res.render('login.html')
+})
+
+router.post('/index', function (req, res) {
+    Account.find(req.body,function(err,data){
+        if(err){
+            console.log(err)
+        } else {
+            if(data.length === 0){
+                
+            } else {
+                res.render('index.html')
+            }
+        }
+    })
+})
+
 module.exports = router
