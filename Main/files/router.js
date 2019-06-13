d3b1c47b0bd75952e5d79d434e4af639d9b9e247
LittleControl <littlecontrol@qq.com>
2019-06-13 21:19:03 +0800

    有关歌曲的增删改查页面

--- Main/files/router.js
+++ Main/files/router.js
@@ -34,6 +34,31 @@ newUser.save(function (err, data) {
     }
 })
 
+var song = mongoose.Schema({
+    name:{
+        required:true,
+        type:String
+    },
+    singer:{
+        required:true,
+        type:String
+    },
+    playNum:{
+        required:true,
+        type:Number
+    },
+    upTime:{
+        type:Date,
+        required:true,
+        default: new Date
+    },
+    commitNum: {
+        type:Number,
+        default:0
+    }
+})
+var Song = mongoose.model('Song',song)
+
 
 var router = express.Router()
 router.get('/', function (req, res) {
@@ -59,7 +84,122 @@ router.post('/index', function (req, res) {
 })
 
 router.get('/dashboard',function(req,res){
-    res.render('dashboard.html')
+    Song.find(function(err,data){
+        if(err){
+            console.log('DataBase Has Some Wrong !')
+        } else {
+
+            res.render('dashboard.html',{
+                songs:data  
+            })
+        }
+    })
 })
+
+router.get('/add',function(req,res){
+    res.render('add.html')
+})
+
+router.post('/add',function(req,res){
+    var newSong = new Song(req.body)
+    newSong.save(function(err,data){
+        if(err){
+            return res.status(500).send(err)
+            // return res.status(500).send('Server Errror !')
+        }
+        res.render('add_success.html')
+    })
+})
+
+router.get('/song_manage',function(req,res){
+    Song.find(function(err,data){
+        if(err){
+            return res.status(500).send('Server Error !')
+        }
+        res.render('song_manage.html',{
+            songs:data
+        })
+    })
+})
+
+router.get('/update',function(req,res){
+    var path = req.originalUrl
+    var id = req.query.id
+    Song.findOne({
+        _id: id
+    }, function (err, data) {
+        if (err) {
+            console.log("Nothing Can Be Find")
+        } else {
+            var song = {}
+            song.id = id
+            song.name = data.name
+            song.singer = data.singer
+            song.upTime = data.upTime
+            song.playNum = data.playNum
+            song.commitNum = data.commitNum
+            song.path = path
+            res.render('update.html', song)
+        }
+    })
+})
+
+router.post('/update',function(req,res){
+    var obj = {
+        id: req.query.id,
+        name: req.body.name,
+        singer: req.body.singer,
+        upTime: JSON.parse(req.body.upTime),
+        playNum: req.body.playNum,
+        commitNum: req.body.commitNum
+    }
+    Song.updateOne({
+        _id: obj.id
+    }, obj, function (err, data) {
+        if (err) {
+            return res.status(500).send("ServeError !")
+        } else {
+            res.render('update_success.html')
+        }
+    })
+})
+
+
+router.get('/delete',function(req,res){
+    var path = req.originalUrl
+    var id = req.query.id
+    Song.findOne({
+        _id: id
+    }, function (err, data) {
+        if (err) {
+            console.log("Nothing Can Be Find")
+        } else {
+            var song = {}
+            song.id = id
+            song.name = data.name
+            song.singer = data.singer
+            song.upTime = data.upTime
+            song.playNum = data.playNum
+            song.commitNum = data.commitNum
+            song.path = path
+            res.render('delete.html', song)
+        }
+    })
+})
+
+router.post('/delete',function(req,res){
+    var id = req.query.id
+    Song.deleteOne({
+        _id: id
+    }, obj, function (err, data) {
+        if (err) {
+            return res.status(500).send("Server Error !")
+        } else {
+            res.render('delete_success.html')
+        }
+    })
+})
+
+
 
 module.exports = router
